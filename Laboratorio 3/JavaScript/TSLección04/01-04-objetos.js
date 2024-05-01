8fb324a0e551a110b576e726febfa1feca2b9163
Lassa <nelsonlassa63@gmail.com>
2024-05-01 19:29:03 -0300

    Clase 4 JavaScript video 1.6 ejecucion desde el navegador mas un poco de css

--- Laboratorio 3/JavaScript/TSLección04/01-04-objetos.js
+++ Laboratorio 3/JavaScript/TSLección04/01-04-objetos.js
@@ -1,5 +1,6 @@
 let x = 10; // variable de tipo primitivo
 console.log(x.length);
+console.log("Tipos Primitivos");
 
 // Objeto
 let persona = {
@@ -24,16 +25,19 @@ persona2.nombre = "Juan";
 persona2.direccion= "Salada 14";
 persona2.telefono = "5492618282821";
 console.log(persona2.telefono);
-
+console.log("Creamos un nuevo objeto")
 console.log(persona["apellido"]); // Accedemos como si fuera un arreglo
-    
+
+console.log("Usamos el ciclo for in");
 //for in y accdemos al objeto como si fuera un arreglo
 for(propiedad in persona){
     console.log(propiedad);
     console.log(persona[propiedad]);
 }
 
-
+console.log("Cambiamos y eliminamos un error");
 persona.apellida = "Betancutd"; // Cambiar dinamicamente el valor del objeto
 delete persona.apellida; // Eliminamos el error
 console.log(persona);
+
+
