6f49dff00bdabfaf7ef97f99e0f45a243afda8a1
Lassa <nelsonlassa63@gmail.com>
2024-05-01 18:26:42 -0300

    Clase 4 JavaScript video 1 y 2 Objetos

---
+++ Laboratorio 3/JavaScript/TSLección04/01-04-objetos.js
@@ -0,0 +1,16 @@
+let x = 10; // variable de tipo primitivo
+console.log(x.length);
+
+// Objeto
+let persona = {
+    nombre: "Carlos",
+    apellido: "Gil",
+    email: "cgil@gmail.com",
+    edad: 30,
+}
+
+console.log(persona.nombre);
+console.log(persona.apellido);
+console.log(persona.email);
+console.log(persona.edad);
+console.log(persona);
