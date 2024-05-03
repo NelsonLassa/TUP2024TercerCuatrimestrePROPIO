b6b2100b65c55185d9721f44e95e3cc8538ae8ed
Lassa <nelsonlassa63@gmail.com>
2024-05-03 19:19:53 -0300

    Clase 3 video 15.7 Sobrecarga de Operadores: Parte 2b

--- Laboratorio 3/Python/Lección12/Persona.py
+++ Laboratorio 3/Python/Lección12/Persona.py
@@ -4,13 +4,17 @@ class Persona:
         self.edad = edad
 
     # TODO sobre escribimos el método add
-    def __add__(self, other): # other significa otro
+    def __add__(self, other):  # other significa otro
         return f'{self.nombre} {other.nombre}'
+
+    def __sub__(self, otro):  # Sub significa = subtraction (resta)
+        return self.edad - otro.edad
 
 
 persona1 = Persona("Ariel", 40)
-persona2 = Persona("Betancud",5)
+persona2 = Persona("Betancud", 5)
 
 # persona1.__add__(persona2) sintaxis interna y automatica
 
-print(persona1+persona2)
+print(persona1 + persona2)
+print(persona1-persona2)
