420e2631c37bda22e0e4a7a61736d70a64b0f554
Lassa <nelsonlassa63@gmail.com>
2024-05-05 23:31:52 -0300

    Clase 3 POO parte 9 video 16.6 Prueba de errores metodo isintence()

--- Laboratorio 3/Python/Lección13/test_polimorfismo.py
+++ Laboratorio 3/Python/Lección13/test_polimorfismo.py
@@ -1,13 +1,17 @@
 from Empleado import Empleado
+
 from Gerente import Gerente
 
 
 def imprimir_detalles(objeto):
     # print(objeto) # De manera indirecta llama al str de la clase Empleado o Gerente
-    print(type(objeto)) # Esto es para saber el tipo de dato que recibe
+    print(type(objeto))  # Esto es para saber el tipo de dato que recibe
     print(objeto.mostrar_detalles())
+    if isinstance(objeto, Gerente):
+        print(objeto.departamento)
 
-empleado = Empleado("Nelson",500000.00)
+
+empleado = Empleado("Nelson", 500000.00)
 imprimir_detalles(empleado)
 
 gerente = Gerente("Valentin", 70000.00, 'Sistemas')
