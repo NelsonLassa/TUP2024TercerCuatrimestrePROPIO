1b944df8cd6504289f1c179e50110bbf436a6e77
Lassa <nelsonlassa63@gmail.com>
2024-05-05 21:27:55 -0300

    Clase 4 Java 1.3 Polimorfismo parte 1

--- Programación 3/Lección04/Sobreescritura/src/main/java/test/TestSobreescritura.java
+++ Programación 3/Lección04/Sobreescritura/src/main/java/test/TestSobreescritura.java
@@ -1,11 +1,21 @@
 package test;
 
+import domain.*;
 import domain.Gerente;
 
 public class TestSobreescritura {
     public static void main(String[] args) {
-        Gerente gerente1 = new Gerente("Nelson",2000,"Sistema");
-        System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+        Empleado empleado1 = new Empleado("Juan", 1000);
+        imprimir(empleado1);
+        //System.out.println("empleado1 = " + empleado1.obtenerDetalles());
 
+        Gerente gerente1 = new Gerente("Nelson", 2000, "Sistema");
+        //System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+        imprimir(gerente1);
+    }
+
+    //Polimorfismo
+    public static void imprimir(Empleado empleado){
+        System.out.println("empleado = " + empleado.obtenerDetalles());
     }
 }
