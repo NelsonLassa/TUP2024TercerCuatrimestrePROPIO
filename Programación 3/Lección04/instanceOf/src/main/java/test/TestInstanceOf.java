ac536f601dbb560cf6f46dd42615a87445db59ac
Lassa <nelsonlassa63@gmail.com>
2024-05-05 22:32:03 -0300

    Clase 4 Java 1.6 Ejercicio Istance of Parte 1

--- Programación 3/Lección04/instanceOf/src/main/java/test/TestInstanceOf.java
+++ Programación 3/Lección04/instanceOf/src/main/java/test/TestInstanceOf.java
@@ -15,9 +15,12 @@ public class TestInstanceOf {
     public static void determinarTipo(Empleado empleado) {
         if (empleado instanceof Gerente) {
             System.out.println("Es de tipo Gerente");
+            Gerente gerente = (Gerente) empleado;
+            gerente.getDepartamento();
         }
         else if (empleado instanceof Empleado) {
             System.out.println("Es de tipo empleado");
+
         }
         else if (empleado instanceof Object) {
             System.out.println("Es de tipo Object");
