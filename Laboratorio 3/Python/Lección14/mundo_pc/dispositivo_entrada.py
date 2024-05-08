3cf3d0e229a421cfec38dbf22c4880f25c90346d
Lassa <nelsonlassa63@gmail.com>
2024-05-08 20:26:41 -0300

    Clase 4 Python video 17.4 Creamos la clase hija: Raton

--- Laboratorio 3/Python/Lección14/mundo_pc/dispositivo_entrada.py
+++ Laboratorio 3/Python/Lección14/mundo_pc/dispositivo_entrada.py
@@ -1,4 +1,4 @@
 class DispositivoEntrada:
     def __init__(self, marca , tipo_entrada):
         self._marca = marca
-        self._tipo_Entrada = tipo_entrada
+        self._tipo_entrada = tipo_entrada
