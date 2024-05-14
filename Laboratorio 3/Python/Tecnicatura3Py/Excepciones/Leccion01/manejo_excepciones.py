b78218b0d4aef17a76f6d6071097417d45f3d94f
Lassa <nelsonlassa63@gmail.com>
2024-05-14 00:48:07 -0300

    Clase 5 Python creacion de clases de Excepcion personalizada Video 7

--- Laboratorio 3/Python/Tecnicatura3Py/Excepciones/Leccion01/manejo_excepciones.py
+++ Laboratorio 3/Python/Tecnicatura3Py/Excepciones/Leccion01/manejo_excepciones.py
@@ -1,8 +1,12 @@
+from NumerosiIgualesException import NumerosIgualesException
+
 resultado = None
 
 try:
     a = int(input('Digite el primer número: '))
     b = int(input("Digite el segundo número: "))
+    if a == b:
+        raise NumerosIgualesException('Son números iguales')
     resultado = a / b  # modificamos
 except TypeError as e:
     print(f'TypeError Ocurrió un error: {type(e)}')
