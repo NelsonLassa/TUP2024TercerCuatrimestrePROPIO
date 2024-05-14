790b54088dd855563cd3865eb4228ad67307b955
Lassa <nelsonlassa63@gmail.com>
2024-05-14 00:33:07 -0300

    Clase 5 Python Procesamiento de excepcion mas especifica Video 4

--- Laboratorio 3/Python/Tecnicatura3Py/Excepciones/Leccion01/manejo_excepciones.py
+++ Laboratorio 3/Python/Tecnicatura3Py/Excepciones/Leccion01/manejo_excepciones.py
@@ -1,10 +1,14 @@
 resultado  = None
-a = '10'
+a = 7
 b = 0
 try:
     resultado = a/b # modificamos
+except TypeError as e:
+    print(f'TypeError Ocurrió un error: {type(e)}')
+except ZeroDivisionError as e:
+    print(f'ZeroDivisionError Ocurrió un error: {type(e)}')
 except Exception as e:
-    print(f'Ocurrió un error: {e}')
+    print(f'Ocurrió un error: {type(e)}')
 
 print(f'El resultado es: {resultado}')
 print('seguimos...')
