f59886e2bac06f84df7f8eb2679a03fd10b30395
Lassa <nelsonlassa63@gmail.com>
2024-05-20 23:33:09 -0300

    Clase 6 Python Manejo de archivos Introduccion a lo que es el manejo de archivos video 2

--- Laboratorio 3/Python/Tecnicatura3Py/Archivos/Leccion02/manejo_archivos.py
+++ Laboratorio 3/Python/Tecnicatura3Py/Archivos/Leccion02/manejo_archivos.py
@@ -1,6 +1,8 @@
 # Derclaramos una variable
 try:
     archivo = open('prueba.txt', 'w')  # La w es de write que significa escribir
+    archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n')
+    archivo.write('Con esto terminamos')
 except Exception as e:
     print(e)
 finally: # siempre se ejecuta
