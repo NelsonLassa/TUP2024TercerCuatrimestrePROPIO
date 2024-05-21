add1823faac88f62ea8f3c2838c72f676ef12ea0
Lassa <nelsonlassa63@gmail.com>
2024-05-21 19:06:06 -0300

    Clase 5 JavaScript with archivos y contexto Manager Parte 1 video 10

--- Laboratorio 3/Python/Tecnicatura3Py/Archivos/Leccion02/asrchivos_con_with.py
+++ Laboratorio 3/Python/Tecnicatura3Py/Archivos/Leccion02/asrchivos_con_with.py
@@ -1,2 +1,6 @@
-
-open()
+# Manejo de contexto with: sintaxis simplificada, abre y cierra el archivo
+with open('prueba.txt','r', encoding='utf8') as archivo:
+    print(archivo.read())
+# No hace falta ni el try, ni el finally
+# en el contexto de with lo que se ejecuta de manera automatica
+# Ahora el siguiente método es el que cierra: __exit__
