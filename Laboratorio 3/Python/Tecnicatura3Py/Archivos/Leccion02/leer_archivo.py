ad039b0b54d7b480dda3a286de65bb65801df80b
Lassa <nelsonlassa63@gmail.com>
2024-05-21 00:47:14 -0300

    Clase 6 Python Manejo de archivos, mas formas de trabajar con archivos video 8

--- Laboratorio 3/Python/Tecnicatura3Py/Archivos/Leccion02/leer_archivo.py
+++ Laboratorio 3/Python/Tecnicatura3Py/Archivos/Leccion02/leer_archivo.py
@@ -1,8 +1,13 @@
-#c:\\usuario\\Nelson\\TUP3°Semestre\\Laboratorio3\\Python\\Tecnicatura3Py\\Archivos\\Leccion02\\prueba.txt si no se encuentra en la misma carpeta
-#se debe colocar la url
-archivo = open('prueba.txt','r', encoding='utf8') #
-#print(archivo.read())
-#print(archivo.read(16))
-#print(archivo.read(10)) # Continiamos desde la línea anterior
-print(archivo.readline())
-print(archivo.readline())
+# c:\\usuario\\Nelson\\TUP3°Semestre\\Laboratorio3\\Python\\Tecnicatura3Py\\Archivos\\Leccion02\\prueba.txt si no se encuentra en la misma carpeta
+# se debe colocar la url
+archivo = open('prueba.txt', 'r', encoding='utf8')  #
+# print(archivo.read())
+# print(archivo.read(16))
+# print(archivo.read(10)) # Continiamos desde la línea anterior
+# print(archivo.readline())
+# print(archivo.readline())
+
+# vamos a iterar el archivo, cada una de las líneas
+#for linea in archivo:
+    # print(linea): Iteramos todos los elementos del archivo
+print(archivo.readlines()[11]) #  accedemos al archivo como si fuera una lista
--- Laboratorio 3/Python/Tecnicatura3Py/Archivos/Leccion02/manejo_archivos.py
+++ Laboratorio 3/Python/Tecnicatura3Py/Archivos/Leccion02/manejo_archivos.py
@@ -5,7 +5,8 @@ try:
     archivo.write('Los acentos son importantes en las palabras\n')
     archivo.write('como por ejemplo: acción, ejecución y producción\n')
     archivo.write('las letras son:\nr read leer,\na append agrega,\nw write escribe,\nx crea un archivo')
-    archivo.write('\nt esta es para texto o text,\nb archivos binarios,\nw+ lee y escribe son iguales r+\n  ')
+    archivo.write('\nt esta es para texto o text,\nb archivos binarios,\nw+ lee y escribe son iguales r+\n')
+    archivo.write('Saludos a todos los alumnos de la tecnicatura\n')
     archivo.write('Con esto terminamos')
 except Exception as e:
     print(e)
