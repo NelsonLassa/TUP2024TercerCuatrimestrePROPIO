31c8c12f8fdf7db6d1c1e09947b8e25ce0534815
Lassa <nelsonlassa63@gmail.com>
2024-05-21 20:27:15 -0300

    Clase 6 javaScript Metodo set video 4

--- Laboratorio 3/JavaScript/TSLección05/05-03-Clases.js
+++ Laboratorio 3/JavaScript/TSLección05/05-03-Clases.js
@@ -7,11 +7,34 @@ class Persona{
     get nombre(){
         return this._nombre;
     }
-    
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+    get apellido(){
+        return this._apellido;
+    }
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
 }
 
 let persona1 = new Persona('Martín','Perez');
 console.log(persona1.nombre);
+persona1.nombre = 'Juan Carlos';
+console.log(persona1.nombre);
+
+// Ejercicio dispuesto
+console.log(persona1.apellido);
+persona1.apellido = 'Sanchez';
+console.log(persona1.apellido);
+//console.log(persona1);
 
 let persona2 = new Persona('Carlos','Lara');
 console.log(persona2.nombre);
+persona2.nombre = 'Maria Laura';
+console.log(persona2.nombre);
+//console.log(persona2);
+// Ejercicio dispuesto
+console.log(persona2.apellido);
+persona2.apellido = 'Castro';
+console.log(persona2.apellido);
