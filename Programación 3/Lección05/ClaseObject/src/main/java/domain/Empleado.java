aae397a4feb9f279099ea13a019a8ffe825d6fbb
Lassa <nelsonlassa63@gmail.com>
2024-06-08 20:36:54 -0300

    Clase 5 Java Hashcode y equals video 5

--- Programación 3/Lección05/ClaseObject/src/main/java/domain/Empleado.java
+++ Programación 3/Lección05/ClaseObject/src/main/java/domain/Empleado.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Empleado {
     protected String nombre;
     protected double sueldo;
@@ -33,5 +35,50 @@ public class Empleado {
     @Override
     public String toString() {
         return "Empleado{" + "nombre= " + nombre + ", sueldo= " + sueldo + " }";
+    }
+
+//    @Override
+//    public int hashCode() {
+//        int hash = 7;
+//        hash = 53 * hash + Objects.hashCode(this.nombre);
+//        hash = 53 * hash + (int) (Double.doubleToLongBits(this.sueldo) ^ (Double.doubleToLongBits(this.sueldo) >>> 32));
+//        return hash;
+//    }
+
+    // Override con netbens incompleto
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj) {
+//            return true;
+//        }
+//        if (obj == null) {
+//            return false;
+//        }
+//        if (getClass() != obj.getClass()) {
+//            return false;
+//        }
+//        final Empleado other = (Empleado) obj;
+//        if (Double.doubleToLongBits(this.sueldo)) != Double.doubleToLongBitst(other.sueldo) {
+//
+//        }
+//        if (!Objects.equals(this.nombre,other.nombre)){
+//            return false;
+//        }
+//        return true;
+//    }
+
+
+    //TODO hashCode y equals Generado con ID
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Empleado empleado = (Empleado) o;
+        return Double.compare(empleado.sueldo, sueldo) == 0 && Objects.equals(nombre, empleado.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
     }
 }
--- Programación 3/Lección05/ClaseObject/src/main/java/test/TestClaseObject.java
+++ Programación 3/Lección05/ClaseObject/src/main/java/test/TestClaseObject.java
@@ -11,15 +11,20 @@ public class TestClaseObject {
         Empleado empleado2 = new Empleado("Jorge",4514);
 
         if (empleado1==empleado2){
-            JOptionPane.showMessageDialog(null,"Tienen la mismma referencia en memoria");
+            JOptionPane.showMessageDialog(null,"1. Tienen la mismma referencia en memoria");
         }else {
-            JOptionPane.showMessageDialog(null,"Tienen distinta referencia en memoria");
+            JOptionPane.showMessageDialog(null,"1. Tienen distinta referencia en memoria");
         }
 
         if (empleado1.equals(empleado2)){
-            JOptionPane.showMessageDialog(null,"Los objetos son iguales en contenido");
+            JOptionPane.showMessageDialog(null,"2. Los objetos son iguales en contenido");
         }else {
-            JOptionPane.showMessageDialog(null,"Los objetos son distintos en contenido");
+            JOptionPane.showMessageDialog(null,"2. Los objetos son distintos en contenido");
+        }
+        if (empleado1.hashCode() == empleado2.hashCode()){
+            JOptionPane.showMessageDialog(null,"3. El valor hashCode es igual");
+        }else {
+            JOptionPane.showMessageDialog(null,"3. El valor hashCode es diferente");
         }
     }
 }
