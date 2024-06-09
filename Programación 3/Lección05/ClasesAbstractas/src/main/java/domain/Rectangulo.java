a9960c022261ce9ecfcfe5695da1c19f87a71854
Lassa <nelsonlassa63@gmail.com>
2024-06-08 21:12:45 -0300

    Clase 5 Java clase Abstractas test y verificacion video 8

--- Programación 3/Lección05/ClasesAbstractas/src/main/java/domain/Rectangulo.java
+++ Programación 3/Lección05/ClasesAbstractas/src/main/java/domain/Rectangulo.java
@@ -1,8 +1,19 @@
 package domain;
 
-public class Rectangulo extends FiguraGeometrica{
+import javax.swing.*;
 
-    void dibujar(){
-        // Comportamiento de la subclase
+public class Rectangulo extends FiguraGeometrica {
+
+    // Constructor
+    public Rectangulo(String tipoFigura) {
+        super(tipoFigura);
     }
+
+    @Override
+    public void dibujar() { // Implementando el método
+        JOptionPane.showMessageDialog(null, "Se imprime un: " + this.getClass().getSimpleName());
+    }
+//    void dibujar(){
+//        // Comportamiento de la subclase
+//    }
 }
