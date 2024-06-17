97ea9389fa06881d478b67e1aa2b076d4ee2317b
Lassa <nelsonlassa63@gmail.com>
2024-06-17 15:35:04 -0300

    Clase 9b Python manejo de transacciones parte 2 - video 4

--- Laboratorio 3/Python/Tecnicatura3Py/Leccion04/DB/transacciones.py
+++ Laboratorio 3/Python/Tecnicatura3Py/Leccion04/DB/transacciones.py
@@ -8,21 +8,21 @@ conexion = bd.connect(
     database='test_bd'
 )
 try:
-    conexion.autocommit = False
+    # conexion.autocommit = False # esto directamente no deberia estar
     cursor = conexion.cursor()
     sentencia = 'INSERT INTO persona(nombre, apellido,email) VALUES (%s, %s, %s)'
-    valores = ('Maria','Esparza','mesparza@mail.com')
-    cursor.execute(sentencia,valores)
+    valores = ('Maria', 'Esparza', 'mesparza@mail.com')
+    cursor.execute(sentencia, valores)
+    conexion.commit()  # Hacemos el commit manualmente
     print('Termina la transacción')
 
 
 except Exception as e:
-    print(f'Ocurrio un error: {e}')
+    conexion.rollback()
+    print(f'Ocurrio un error, se hizo un rollback: {e}')
 finally:
     # cursor.close() el cierre del cursor lo esta haciendo el with
     conexion.close()
-
-
 
 # Documentación de la conexion a la base de datos
 # https://www.psycopg.org/docs/usage.html
