31a11b712c3d01ba9e8e4d07ec57052c0d057c7b
Lassa <nelsonlassa63@gmail.com>
2024-06-17 11:16:13 -0300

    Clase 9a Python Funcion fetchall en pyscopg2 parte 1 - video 3

--- Laboratorio 3/Python/Tecnicatura3Py/Leccion04/DB/v_registros.py
+++ Laboratorio 3/Python/Tecnicatura3Py/Leccion04/DB/v_registros.py
@@ -11,18 +11,17 @@ try:
     with conexion:
         with conexion.cursor() as cursor:
             # cursor = conexion.cursor() # Esto ya esta haciendolo el with
-            sentencia = 'SELECT * FROM persona WHERE id_persona = %s' # Placeholder
-            id_persona = input('Digite un número para el id_ perosona: ')
-            cursor.execute(sentencia,(id_persona,))  # De esta manera ejecutamos la sentencia
-            registros = cursor.fetchone()  # Recuperamos todos los reregistros que seran una lista
-            print(registros)
+            sentencia = 'SELECT * FROM persona WHERE id_persona IN (1,2)'  # Placeholder
+            #id_persona = input('Digite un número para el id_ perosona: ')
+            cursor.execute(sentencia)  # De esta manera ejecutamos la sentencia
+            registros = cursor.fetchall()  # Recuperamos todos los reregistros que seran una lista
+            for registro in registros:
+                print(registros)
 except Exception as e:
     print(f'Ocurrio un error: {e}')
 finally:
     # cursor.close() el cierre del cursor lo esta haciendo el with
     conexion.close()
-
-
 
 # Documentación de la conexion a la base de datos
 # https://www.psycopg.org/docs/usage.html
