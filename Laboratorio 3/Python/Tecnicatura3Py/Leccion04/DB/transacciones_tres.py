ec54d48accf8709cbd4ea335d021ef361a2f97b5
Lassa <nelsonlassa63@gmail.com>
2024-06-17 16:01:17 -0300

    Clase 9b Python manejo de transacciones con with - video 7

---
+++ Laboratorio 3/Python/Tecnicatura3Py/Leccion04/DB/transacciones_tres.py
@@ -0,0 +1,29 @@
+import psycopg2 as bd  # Esto es para poder conectarnos a PostgreSQL
+
+conexion = bd.connect(
+    user='postgres',
+    password='admin',
+    host='127.0.0.1',
+    port='5432',
+    database='test_bd'
+)
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'INSERT INTO persona(nombre, apellido,email) VALUES (%s, %s, %s)'
+            valores = ('Alex', 'Rojas', 'arojas@mail.com')
+            cursor.execute(sentencia, valores)
+
+            sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+            valores = ('Juan Carlos', 'Roldan','jcroldan@mail.com',1)
+            cursor.execute(sentencia,valores)
+
+except Exception as e:
+    print(f'Ocurrio un error, se hizo un rollback: {e}')
+finally:
+    # cursor.close() el cierre del cursor lo esta haciendo el with
+    conexion.close()
+
+print('Termina la transacción')
+
+
