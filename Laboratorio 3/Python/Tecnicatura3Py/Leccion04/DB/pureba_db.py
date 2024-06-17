32eefe6ebd482fd543161c0ff9800c7aa49d7d5a
Lassa <nelsonlassa63@gmail.com>
2024-06-17 11:03:59 -0300

    Clase 9a Python Uso de with y pysycopg2 video 1

--- Laboratorio 3/Python/Tecnicatura3Py/Leccion04/DB/pureba_db.py
+++ Laboratorio 3/Python/Tecnicatura3Py/Leccion04/DB/pureba_db.py
@@ -1,18 +1,25 @@
-import psycopg2 # Esto es para poder conectarnos a PostgreSQL
+import psycopg2  # Esto es para poder conectarnos a PostgreSQL
 
 conexion = psycopg2.connect(
-    user = 'postgres',
-    password = 'admin',
-    host = '127.0.0.1',
-    port = '5432',
+    user='postgres',
+    password='admin',
+    host='127.0.0.1',
+    port='5432',
     database='test_bd'
 )
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            # cursor = conexion.cursor() # Esto ya esta haciendolo el with
+            sentencia = 'SELECT * FROM persona'
+            cursor.execute(sentencia)  # De esta manera ejecutamos la sentencia
+            registros = cursor.fetchall()  # Recuperamos todos los reregistros que seran una lista
+            print(registros)
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
+finally:
+    # cursor.close() el cierre del cursor lo esta haciendo el with
+    conexion.close()
 
-cursor = conexion.cursor()
-sentencia = 'SELECT * FROM persona'
-cursor.execute(sentencia) # De esta manera ejecutamos la sentencia
-registros = cursor.fetchall() # Recuperamos todos los reregistros que seran una lista
-print(registros)
-
-cursor.close()
-conexion.close()
+# Documentación de la conexion a la base de datos
+# https://www.psycopg.org/docs/usage.html
